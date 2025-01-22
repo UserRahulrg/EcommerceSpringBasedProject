5cc1ab5dcd9b527d7391c51d0b4acf03e603e2d1
Rahul <newrahulgautam@gmail.com>
2025-01-22 12:30:14 +0530

    Update25.01.22.01

---
+++ src/main/java/com/projectspring/ecommerceweb/EcommercewebApplication.java
@@ -0,0 +1,17 @@
+package com.projectspring.ecommerceweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercewebApplication.class, args);
+		
+		System.out.println("");
+		System.out.println("EcommerceSpringBootBased WebApplication!!");
+	}
+
+	
+}
